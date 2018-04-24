7a29208c5352416ad72e0cd9e4dbc6497501eafb
luqiwei <luqiwei@kjt.com.cn>
2018-04-24 16:30:39 +0800

    tesng干å二次开发

--- casedemo/src/main/java/com/course/config/TestConfig.java
+++ casedemo/src/main/java/com/course/config/TestConfig.java
@@ -4,6 +4,7 @@ import lombok.Data;
 import org.apache.http.client.CookieStore;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.client.HttpClients;
 
 
 @Data
@@ -24,6 +25,6 @@ public class TestConfig {
     //用来存储cookies信息的变量
     public static CookieStore store;
     //声明http客户端
-    public static CloseableHttpClient httpClient;
+    public static CloseableHttpClient httpClient= HttpClients.custom().setDefaultCookieStore(store).build();
 
 }
